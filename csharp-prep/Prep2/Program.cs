85b6bf942f5ad464c580b43a15a4c07913f76683
Okocha <oko23015@byui.edu>
2024-11-04 04:49:20 -0800

    My second work

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your percentage score?");
+
+        string myScore = Console.ReadLine();
+        int percentageScore = int.Parse(myScore);
+
+        if (percentageScore >= 90)
+        {
+            Console.WriteLine("You made an A");
+        }
+
+        
+        else if (percentageScore >= 80)
+        {
+            Console.WriteLine("You made a B");
+
+        }
+        else if (percentageScore >= 70)
+        {
+            Console.WriteLine("You made a C");
+
+        }
+        else
+        {
+            Console.WriteLine("You made a F");
+        }
+
+        if (percentageScore >= 70)
+        {
+            Console.WriteLine("Congratulations you made it.");
+        }
+        else 
+        {
+            Console.WriteLine("Put more efforts next time.");
+        }
     }
 }
