0a6c8fa18c24ae7edde412547999017835bd5a0b
Okocha <oko23015@byui.edu>
2024-11-05 01:05:12 -0800

    My second prep3 work

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -7,25 +7,27 @@ class Program
         
         Console.WriteLine("What is the secret number?");
         int secretNumber = int.Parse(Console.ReadLine());
-        Console.WriteLine("What is your guess?");
 
-        int myGuess = int.Parse(Console.ReadLine());
+        int myGuess = -1; // Initialize myGuess to a value that won't match secretNumber initially
 
-    
+        while (myGuess != secretNumber)
+        {
+            Console.WriteLine("What is your guess?");
+            myGuess = int.Parse(Console.ReadLine());
 
-        if (myGuess == secretNumber)
-        {
-            Console.WriteLine("Congratulations, you guessed it!");
+            if (myGuess == secretNumber)
+            {
+                Console.WriteLine("Congratulations, you guessed it!");
+            }
+            else if (myGuess > secretNumber)
+            {
+                Console.WriteLine("Try guessing lower.");
+            }
+            else
+            {
+                Console.WriteLine("Try guessing higher.");
+            }
         }
-        else if (myGuess > secretNumber)
-        {
-            Console.WriteLine("Try guessing lower.");
-        }
-        else
-        {
-            Console.WriteLine("Try guessing higher.");
-        }
-        
         
     }
 
