ba8a0c8c14256cadf18c9335c113c65259bcec6f
Okocha <oko23015@byui.edu>
2024-11-05 01:00:08 -0800

    My first prep3 work

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        
+        Console.WriteLine("What is the secret number?");
+        int secretNumber = int.Parse(Console.ReadLine());
+        Console.WriteLine("What is your guess?");
+
+        int myGuess = int.Parse(Console.ReadLine());
+
+    
+
+        if (myGuess == secretNumber)
+        {
+            Console.WriteLine("Congratulations, you guessed it!");
+        }
+        else if (myGuess > secretNumber)
+        {
+            Console.WriteLine("Try guessing lower.");
+        }
+        else
+        {
+            Console.WriteLine("Try guessing higher.");
+        }
+        
+        
     }
+
 }
