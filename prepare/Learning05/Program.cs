db711c094bb30f91615414a71ef91f1d8b567349
Okocha <oko23015@byui.edu>
2024-12-15 08:04:43 -0800

    my week 5 work

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -10,7 +10,19 @@ class Program
         Console.WriteLine(summary);
 
         
+        // Create a MathAssignment object
+        MathAssignment mathAssignment = new MathAssignment("Chimaobi Okoro", "Fractions", "7.3", "8-19");
+
+        // Display the summary
+        Console.WriteLine(mathAssignment.GetSummary());
+
+        // Display the homework list
+        Console.WriteLine(mathAssignment.GetHomeworkList());
         
+        WritingAssignment writingAssignment = new WritingAssignment("Mary Jane", "European History", "The Fall of Rome");
+
+        // Display the writing information
+        Console.WriteLine(writingAssignment.GetWritingInformation());
         
         
 
--- prepare/Learning05/WritingAssignment.cs
+++ prepare/Learning05/WritingAssignment.cs
@@ -1,14 +1,21 @@
+public class WritingAssignment : Assignment
+{
+    // Private field for the title
+    private string _title;
 
-// public class WritingAssignment : Assignment
-// {
-//     private string _title = "";
+    // Constructor initializing base and new attributes
+    public WritingAssignment(string studentName, string topic, string title) 
+        : base(studentName, topic)
+    {
+        _title = title;
+    }
 
-//     public string GetWritingInformation()
-//     {
-//         Console.WriteLine();
-//     }
-    
+    // Method to get writing information
+    public string GetWritingInformation()
+    {
+        // Using the GetStudentName() method because _studentName is private in the base class
+        string studentName = GetStudentName();
 
-
-
-// }
+        return $"{_title} by {studentName}";
+    }
+}
